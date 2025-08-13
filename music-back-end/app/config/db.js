6eb88f1d005aa8cf87cd922ba70054d584493391
Thomas Lhomme <lhomme.thomas@gmail.com>
2025-08-13 09:14:51 +0800

    new password

--- music-back-end/app/config/db.js
+++ music-back-end/app/config/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2/promise';
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'passw0rd',
+    password: 'myPa55word',
     database: 'music_db',
 });
 
